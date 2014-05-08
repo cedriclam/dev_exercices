0ef1938a2582d40f63130c8184225ffadc42446d
Cedric Lamoriniere <cedric.lamoriniere@gmail.com>
2014-05-08 22:55:07 +0200

    start exercice3-3

---
+++ src/dev_exercices/SetOfStacks.h
@@ -0,0 +1,31 @@
+
+#ifndef SRC_DEV_EXERCICES_SETOFSTACKS_H_
+#define SRC_DEV_EXERCICES_SETOFSTACKS_H_
+
+#include <list>
+
+#include "dev_exercices/Stack.h"
+
+/** @namespace dev_exercices */
+namespace dev_exercices {
+class Node;
+
+class SetOfStacks{
+  public:
+    SetOfStacks();
+    virtual ~SetOfStacks();
+
+    void push(int iData);
+    void push(Node* ipNode);
+    Node* pop();
+    int pop_data();
+
+    inline int nb_stacks() { return _stacks.size(); };
+
+  private:
+    std::list<Stack* > _stacks;
+};
+
+}  // namespace dev_exercices
+
+#endif  // SRC_DEV_EXERCICES_SETOFSTACKS_H_
